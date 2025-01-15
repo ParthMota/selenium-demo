89d73d588e06fcda322d65eccd17f775c78d5787
Parth Mota <motaparth@gmail.com>
2025-01-15 22:10:04 +0530

    New Commit

---
+++ SeleniumTraining/src/main/java/WriteIntoExcel.java
@@ -0,0 +1,10 @@
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class WriteIntoExcel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		XSSFWorkbook workbook = new XSSFWorkbook();
+	}
+
+}
