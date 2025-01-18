c1a73eed4884064d244084c942b94a8aa176c3f3
Parth Mota <motaparth@gmail.com>
2025-01-18 14:06:02 +0530

    Commit Write into Excel

--- SeleniumTraining/src/main/java/WriteIntoExcel.java
+++ SeleniumTraining/src/main/java/WriteIntoExcel.java
@@ -1,3 +1,8 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WriteIntoExcel {
@@ -5,6 +10,11 @@ public class WriteIntoExcel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		XSSFWorkbook workbook = new XSSFWorkbook();
+		
+		XSSFSheet worksheet = workbook.createSheet("samplesheet");
+		
+		Map<String, Object[]> dataset = new TreeMap<String, Object[]>();
+		
 	}
 
 }
